6cf786b001692fa947a43a401b5970e1ca700def
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-03-10 16:43:52 +0000

    updated index, css

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@
 function enterClicked () {
   // this function gets user's street number and name and displays it back to user
 
-	// get age from text field and cast it to integer
+	// get street number from text field and cast it to integer
   let streetNumber = parseInt(document.getElementById("street-number").value)
   // get street name from text field
   let streetName = document.getElementById("street-name").value
-  // display name and age back to user
+  // display street number and name back to user
   document.getElementById('user-info').innerHTML = "You live on " + streetNumber + " " +
  streetName + "."
 }
